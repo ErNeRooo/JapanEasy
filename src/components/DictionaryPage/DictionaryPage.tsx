a029aa9e219c50fdeddb8ce69863f3577cc7f051
ErNeRooo <erneroooooo@gmail.com>
2024-07-29 14:29:01 +0200

    add DictionaryPage

---
+++ src/components/DictionaryPage/DictionaryPage.tsx
@@ -0,0 +1,11 @@
+import styles from "./DictionaryPage.module.sass";
+
+export const DictionaryPage = () => {
+  return (
+    <>
+      <div>nawigacja</div>
+      <div className={styles.wyszukiwanie}></div>
+      <div id="content">...jakies slowa</div>
+    </>
+  );
+};
