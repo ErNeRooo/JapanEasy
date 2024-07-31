0c3aaec9b201c34e5092396b741ee9c2ade27a71
ErNeRooo <erneroooooo@gmail.com>
2024-07-31 15:24:55 +0200

    change layout

---
+++ src/components/NavBar/NavBar.tsx
@@ -0,0 +1,5 @@
+import styles from "./NavBar.module.sass";
+
+export const NavBar = () => {
+  return <div className={styles.NavBar}></div>;
+};
--- src/components/WordsBar/WordsBar.tsx
+++ src/components/WordsBar/WordsBar.tsx
@@ -9,21 +9,16 @@ export const WordsBar = () => {
     ["家", "いえ", "iu", "dom"],
     ["新しい", "あたらしい", "atarashii", "nowe"],
     ["繰り返す", "くりかえす", "kurikaesu", "powtarzać"],
-    ["私", "わたし", "watashi", "Ja"],
-    ["私", "わたし", "watashi", "Ja"],
-    ["私", "わたし", "watashi", "Ja"],
-    ["私", "わたし", "watashi", "Ja"],
+    ["私私私私私", "わたし", "watashi", "Ja"],
+    ["私私私私私私", "わたし", "watashi", "Ja"],
+    ["私私私私私私私", "わたし", "watashi", "Ja"],
+    ["私私私私私私私私", "わたし", "watashi", "Ja"],
   ]);
 
   return (
     <div className={styles.WordsBar}>
       {words.map((word) => (
-        <Word
-          name={word[0]}
-          hiragana={word[1]}
-          romaji={word[2]}
-          definition={word[3]}
-        />
+        <Word name={word[0]} romaji={word[2]} definition={word[3]} />
       ))}
     </div>
   );
