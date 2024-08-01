238953943695ea983d1bb89ae4da69f05816e148
ErNeRooo <erneroooooo@gmail.com>
2024-08-01 15:46:26 +0200

    add SearchBar

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -1,17 +1,10 @@
-import searchIcon from "../../assets/Search-Icon.svg";
-import slidersIcon from "../../assets/sliders-icon.svg";
+import { SearchBar } from "./SearchBar/SearchBar";
 import styles from "./NavBar.module.sass";
 
 export const NavBar = () => {
   return (
     <div className={styles.NavBar}>
-      <div className={styles.searchBar}>
-        <img src={searchIcon} />
-        <input type="text" />
-        <div className={styles.filter}>
-          <img src={slidersIcon} />
-        </div>
-      </div>
+      <SearchBar></SearchBar>
     </div>
   );
 };
