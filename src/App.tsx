70520555606ec3fcf5539e00d4bb76612027eb75
ErNeRooo <erneroooooo@gmail.com>
2024-08-02 13:42:01 +0200

    add firebase

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,26 @@
 import "./App.module.sass";
 import { DictionaryPage } from "./components/DictionaryPage/DictionaryPage";
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import firebase from "firebase/compat/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAuXbqhGOkU9ySIfio0tMvcN4GjXMg6vhg",
+  authDomain: "nihon-go-kaizen.firebaseapp.com",
+  projectId: "nihon-go-kaizen",
+  storageBucket: "nihon-go-kaizen.appspot.com",
+  messagingSenderId: "568822236498",
+  appId: "1:568822236498:web:0e83f37c1c63851227e68b",
+  measurementId: "G-8VE2QR44F3",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
+firebase.initializeApp(firebaseConfig);
 
 function App() {
   return <DictionaryPage></DictionaryPage>;
