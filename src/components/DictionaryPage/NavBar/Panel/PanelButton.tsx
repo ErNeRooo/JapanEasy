a35b2ce66f8ab9e48eb9eee8f0c96211393b7dfb
ErNeRooo <erneroooooo@gmail.com>
2024-08-04 16:48:15 +0200

    add cool onClick rotate for PanelButton

--- src/components/DictionaryPage/NavBar/Panel/PanelButton.tsx
+++ src/components/DictionaryPage/NavBar/Panel/PanelButton.tsx
@@ -1,8 +1,23 @@
+import { useState } from "react";
 import styles from "./PanelButton.module.sass";
 
 export const PanelButton = ({ image }: IProps) => {
-  return (
-    <div className={styles.Panel}>
+  const [isOpen, setIsOpen] = useState(false);
+
+  const HandleOnClick = () => {
+    setIsOpen((prev) => !prev);
+  };
+
+  return isOpen ? (
+    <div
+      className={styles.Panel + " " + styles.Clicked}
+      onClick={HandleOnClick}
+      style={{ transform: "rotate(180deg)" }}
+    >
+      <img src={image} />
+    </div>
+  ) : (
+    <div className={styles.Panel} onClick={HandleOnClick}>
       <img src={image} />
     </div>
   );
