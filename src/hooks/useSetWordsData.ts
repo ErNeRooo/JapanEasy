a0d0943e7f9b3f44e4f448a412b782ad8828d354
ErNeRooo <erneroooooo@gmail.com>
2024-08-15 12:02:03 +0200

    handling no data

--- src/hooks/useSetWordsData.ts
+++ src/hooks/useSetWordsData.ts
@@ -1,6 +1,7 @@
 import { useCallback, useEffect, useState } from "react";
 import wordTypes from "../types/wordTypes";
 import searchTypes from "../types/searchTypes";
+import dictionaryData from "../assets/dictionaryData.json";
 
 let countSeeMoreTriggers = 1;
 const useSetWordsData = ({
@@ -14,76 +15,60 @@ const useSetWordsData = ({
   const [words, setWords] = useState<wordTypes[]>([]);
 
   const setWordsData = useCallback(() => {
-    getWords().then((wordsArray) => {
-      const result = wordsArray
-        .filter((word) => {
-          if (partOfSpeech) {
-            return word.PartOfSpeech.replace(",", "")
-              .split(" ")
-              .includes(partOfSpeech);
-          } else {
-            return word;
-          }
-        })
-        .filter((word) => {
-          if (searchPrompt) {
-            return (
-              word["Rank"].toString() === searchPrompt ||
-              word["Romaji"]
-                .toLowerCase()
-                .includes(searchPrompt.toLowerCase()) ||
-              word["Lemma"].toLowerCase().includes(searchPrompt.toLowerCase())
-            );
-          } else {
-            return word;
-          }
-        })
-        .sort((a, b) => {
-          if (a[field] < b[field]) {
-            return order === "asc" ? -1 : 1;
-          }
-          if (a[field] > b[field]) {
-            return order === "asc" ? 1 : -1;
-          }
-          return 0;
-        })
-        .slice((countSeeMoreTriggers - 1) * 50, countSeeMoreTriggers * 50);
+    const result = getWords()
+      .filter((word) => {
+        if (partOfSpeech) {
+          return word.PartOfSpeech.replace(",", "")
+            .split(" ")
+            .includes(partOfSpeech);
+        } else {
+          return word;
+        }
+      })
+      .filter((word) => {
+        if (searchPrompt) {
+          return (
+            word["Rank"].toString() === searchPrompt ||
+            word["Romaji"].toLowerCase().includes(searchPrompt.toLowerCase()) ||
+            word["Lemma"].toLowerCase().includes(searchPrompt.toLowerCase())
+          );
+        } else {
+          return word;
+        }
+      })
+      .sort((a, b) => {
+        if (a[field] < b[field]) {
+          return order === "asc" ? -1 : 1;
+        }
+        if (a[field] > b[field]) {
+          return order === "asc" ? 1 : -1;
+        }
+        return 0;
+      })
+      .slice((countSeeMoreTriggers - 1) * 50, countSeeMoreTriggers * 50);
 
-      if (countSeeMoreTriggers === 1) setWords(result);
-      else setWords((prev): wordTypes[] => [...prev, ...result]);
-      countSeeMoreTriggers++;
-    });
+    if (countSeeMoreTriggers === 1) setWords(result);
+    else setWords((prev): wordTypes[] => [...prev, ...result]);
+    countSeeMoreTriggers++;
+
+    setIsLoading(false);
   }, [searchPrompt, partOfSpeech, field, order]);
 
   useEffect(() => {
+    setErrorMessage("");
     setWordsData();
     countSeeMoreTriggers = 1;
-  }, [setWordsData]);
+    if (words.length === 0) setErrorMessage("No words found");
+  }, [setWordsData, words, setErrorMessage]);
 
-  const getWords = async (): Promise<wordTypes[]> => {
-    return new Promise<wordTypes[]>((resolve, reject) => {
-      const wordsArray: wordTypes[] = [];
+  const getWords = (): wordTypes[] => {
+    const wordsArray: wordTypes[] = [];
 
-      fetch("../../public/dictionaryData.json")
-        .then((response) => response.json())
-        .then((data) => {
-          if (data.length === 0) {
-            reject();
-            throw new Error("No documents found");
-          }
-
-          data.forEach((doc: wordTypes) => {
-            wordsArray.push(doc);
-          });
-        })
-        .catch((error) => {
-          setErrorMessage(error.message);
-        })
-        .finally(() => {
-          setIsLoading(false);
-          resolve(wordsArray);
-        });
+    dictionaryData.forEach((doc: wordTypes) => {
+      wordsArray.push(doc);
     });
+
+    return wordsArray;
   };
 
   return { words, setWordsData, isLoading, errorMessage };
